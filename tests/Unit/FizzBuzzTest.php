b8381fb7bc6899e7b646d6bd0c8efb100c246327
Alexander Hofmüller <ah@webundsoehne.com>
2021-06-11 12:04:41 +0200

    reports and first failing test

---
+++ tests/Unit/FizzBuzzTest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class FizzBuzzTest extends TestCase
+{
+    /** @test */
+    public function it_returns_fizz_for_multiples_of_three()
+    {
+        // static for now maybe I'll adapt that
+        $this->assertEquals('fizz',  FizzBuzz::check(3));
+    }
+}

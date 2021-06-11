aa1e9144821e352e1a2c4e1834aa0f3618efc8dc
Alexander Hofmüller <ah@webundsoehne.com>
2021-06-11 14:26:08 +0200

    fizz rule updated

--- app/Models/FizzBuzz.php
+++ app/Models/FizzBuzz.php
@@ -6,8 +6,9 @@ class FizzBuzz
 {
     public static function check(int $number)
     {
+        if ($number % 3 === 0)
+            return 'fizz';
         if ($number % 5 === 0)
             return 'buzz';
-        return 'fizz';
     }
 }

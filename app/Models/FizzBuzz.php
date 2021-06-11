7de3e63c377524be9a3c23856681e6eebf155df2
Alexander Hofmüller <ah@webundsoehne.com>
2021-06-11 14:28:50 +0200

    now working

--- app/Models/FizzBuzz.php
+++ app/Models/FizzBuzz.php
@@ -6,6 +6,8 @@ class FizzBuzz
 {
     public static function check(int $number)
     {
+        if ($number % 3 === 0 & $number % 5 === 0)
+            return 'fizzbuzz';
         if ($number % 3 === 0)
             return 'fizz';
         if ($number % 5 === 0)
